0bf1a278c3f1ddb95c19821f9f95f2c9ddbdcb77
BonsonDem <it.bonson.demssie@gmail.com>
2022-01-27 18:06:57 +0300

    code fix and start file

--- footer.php
+++ footer.php
@@ -1,8 +1,3 @@
-<hr>
-<footer class="site-footer">
-    <p><?php bloginfo('name') ?></p>
-</footer>
-<p> Developed by Biruk Tesfaye and Bonson Demssie &copy;2022</p>
-<?php wp_footer() ?>
+<?php wp_footer(); ?>
 </body>
 </html>
