7f94854d3257f1382f2a78535713b72afe580617
biruktesfu <it.biruk.tesfaye@gmail.com>
2022-01-27 21:49:55 -0900

    fixed full post not showing up after excerpt link clicked

--- single.php
+++ single.php
@@ -2,12 +2,12 @@
 
 
 
-<div class="container" align-text="center">
+<div class="container">
     <h1> <?php single_cat_title(); ?></h1>
 
     <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
 
-        <?php the_excerpt(); ?>
+        <?php the_content(); ?>
 
     <?php endwhile; endif;?>
 </div>
