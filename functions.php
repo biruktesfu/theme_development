8bf70d9ef398da04d447793342ddddb101f742bd
BonsonDem <it.bonson.demssie@gmail.com>
2022-01-28 14:15:45 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -47,5 +47,5 @@ register_nav_menus(
 function register_navwalker(){
     require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 }
-add_action( 'after_setup_theme', 'register_navwalker' );
+add_action( 'after_setup_theme', 'register_navwalker' ); 
 
